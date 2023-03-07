77b59a10377bdc034f72379307a0ae999ab395f9
Raquell2005 <raqdezotti@gmail.com>
2023-03-06 22:01:51 -0300

    eeeee

--- cria_produto.php
+++ cria_produto.php
@@ -3,12 +3,23 @@
 include_once 'produtos.php';
 
 $valor = new Produto();
+$valor1 = new Produto();
 
 $valor ->Codigo = 4055;
 $valor ->Descricao = "Boné da Gucci";
+$valor ->Preco = 1000.00;
+$valor ->Quantidade = 3;
 
-echo $valor->Codigo ." - ". $valor->Descricao;
 
+$valor1 ->Codigo = 4065;
+$valor1 ->Descricao = "Camiseta da Hello Kitty";
+$valor1 ->Preco = 300.00;
+$valor1 ->Quantidade = 5;
 
+//echo $valor->Codigo ." - ". $valor->Descricao;
+$valor ->ImprimeEtiqueta();
+$valor1 ->ImprimeEtiqueta();
+$valor ->Desconto();
+$valor1 ->Desconto();
 
  ?>
--- produtos.php
+++ produtos.php
@@ -6,6 +6,35 @@ class Produto
 	var $Descricao;
 	var $Preco;
 	var $Quantidade;
+
+	function ImprimeEtiqueta(){
+
+		print 'Código:' . $this->Codigo . "<br>";
+		print 'Descrição:' . $this->Descricao . "<br>";
+		print 'Preço: ' . $this->Preco . "<br>";
+	}
+
+	function Desconto(){
+		$desc = 0;
+		$total = 00.0;
+		if ($this->Quantidade >= 3 ) {
+			$desc = 10;
+			$total = ($this->Preco - ($this->Preco * $desc / 100)) * $this->Quantidade;
+			
+		} elseif ($this->Quantidade >= 5 ) {
+			$desc = 20;
+			$total = ($this->Preco - ($this->Preco * $desc / 100)) * $this->Quantidade;
+			
+		} elseif ($this->Quantidade >= 7 ) {
+			$desc = 30;
+			$total = ($this->Preco - ($this->Preco * $desc / 100)) * $this->Quantidade;
+			
+		}
+
+		print 'Valor Total com desconto: ' . $total . "<br>";
+
+
+	}
 	
 }
 
